33aad662c99f7ba85c037c600adb17d5fe05c325
Shubham Karmveer <s.karmveer1201@gmail.com>
2023-11-27 11:11:10 +0530

    Add files via upload

---
+++ Python NITKKR/Fibonacci REsult.py
@@ -0,0 +1,3 @@
+import fibonacci  
+num=int(input("Enter any number to print Fibonacci series : ")) 
+fibonacci.fib(num) 
---
+++ Python NITKKR/fibonacci.py
@@ -0,0 +1,8 @@
+def fib(n):
+    a, b = 0, 1 
+    print(0, end =" ") 
+    while b <= n: 
+        print(b, end =" ") 
+        a, b = b, a+b
+
+        
